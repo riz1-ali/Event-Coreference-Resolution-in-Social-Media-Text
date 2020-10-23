30ec22c502f7c6cb7285cfc6a9e1b8782ecc9749
riz1-ali <rizwan16699@gmail.com>
2020-10-23 17:23:05 +0530

    Fix Distance Vecs

--- pair_up_tweets.py
+++ pair_up_tweets.py
@@ -1,11 +1,13 @@
 def gen_distance_vec(tweet,trigger_word):
+	trig = trigger_word.split()
+	trig = trig[len(trig)//2]
+	words = tweet.split()
 	pos = -1
-	t = tweet.split()
-	for i in range(len(t)):
-		if trigger_word == t[i]:
+	for i in range(len(words)):
+		if trig == words[i]:
 			pos = i
 			break
-	return [i-pos for i in range(len(t))]
+	return [i-pos for i in range(len(words))]
 
 tweet_pairs = []
 distance_vecs = []
@@ -15,7 +17,6 @@ with open(file_path,'r') as f:
 	trigger_word = {}
 	for i in f:
 		data = i.strip('\n').split('\t')
-		print(data[3])
 		distance_vecs.append(gen_distance_vec(data[-1],data[3]))
 		if data[3] not in trigger_word.keys():
 			trigger_word[data[3]] = []
@@ -26,5 +27,10 @@ with open(file_path,'r') as f:
 			for i in range(len(trigger_word[word])):
 				for j in range(i+1,len(trigger_word[word])):
 					tweet_pairs.append([trigger_word[word][i],trigger_word[word][j]])
-# for i in tweet_pairs:
-# 	print(i)
+
+f = open('distance_vectors.txt','w')
+f.write(str(distance_vecs))
+f.close()
+f = open('tweet_pairs.txt','w')
+f.write(str(tweet_pairs))
+f.close()
