247e5378b0f906bee9c1b83502a74c67d7ac9905
riz1-ali <rizwan16699@gmail.com>
2020-10-24 23:41:33 +0530

    Modify DataLoader for Tweet Pairs

--- dataloader.py
+++ dataloader.py
@@ -4,23 +4,35 @@ import torch
 from gensim.models import Word2Vec
 
 class dataset(data.Dataset):
-	def __init__(self,input):
-		self.input = input
+	def __init__(self,tweet_pairs,distance_vectors):
+		self.tweet_pairs = tweet_pairs
+		self.distance_vectors = distance_vectors
+		model = Word2Vec.load("word2vec.model")
+		self.word2vec = model.wv.vocab
 	def __len__(self):
-		return len(self.input)
+		return len(self.tweet_pairs)
+	def convert_tweet(self,tweet):
+		return [ self.word2vec[word.strip()] for word in tweet.split()]
 	def __getitem__(self,index):
-		return torch.Tensor(self.input)
+		return torch.Tensor([self.convert_tweet(self.tweet_pairs[index][0]),
+			self.convert_tweet(self.tweet_pairs[index][1])]),
+		torch.Tensor(self.distance_vectors)
+
 
 def collate_fn(batch):
 	return rnn.pack_sequence(batch)
 
-if __name__ == '__main__':
-	model = Word2Vec.load("word2vec.model")
-	a = model.wv.vocab
-	b = [i for i in a.keys()]
-	b = b[-70:]
-	data_ = [model.wv[i] for i in b]
-	data_ = dataset(data_)
-	loader = data.DataLoader(data_, batch_size=32, collate_fn=collate_fn, shuffle=True)
-	# for i in loader:
-	# 	print(i)
+data_ = []
+file_path = './generated_dataset.txt'
+with open(file_path,'r') as f:
+	for i in f:
+		j = i.strip('\n').split('\t')
+		data_.append(j[-1])
+f = open('./tweet_pairs.txt','r')
+tweet_pairs = eval(f.read())
+f = open('./distance_vectors.txt','r')
+distance_vectors = eval(f.read())
+tweet_pair_data = [[data_[i[0]],data_[i[1]]] for i in tweet_pairs]
+distance_vector_data = [[distance_vectors[i[0]],distance_vectors[i[1]]] for i in tweet_pairs]
+# loader = data.DataLoader(data_, batch_size=32, collate_fn=collate_fn, shuffle=True)
+dataset_ = dataset(tweet_pair_data,distance_vector_data)
