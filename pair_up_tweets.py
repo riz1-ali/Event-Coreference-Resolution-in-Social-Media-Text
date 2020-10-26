1053f238a4ae029f5b0a8e322faa1bf9bb16bcf0
riz1-ali <rizwan16699@gmail.com>
2020-10-26 11:11:31 +0530

    Adding Labels for Coreferenced Tweets

--- pair_up_tweets.py
+++ pair_up_tweets.py
@@ -14,14 +14,21 @@ def gen_distance_vec(tweet, trigger_word):
 		if trig[0] == words[i]:
 			first_p = i
 			break
+	if first_p==-1:
+		for i in range(len(words)):
+			if trig[0] in words[i] or words[i] in trig[0]:
+				first_p = i
+				break
 	for i in range(len(words)-1,-1,-1):
 		if trig[-1] == words[i]:
 			last_p = i
 			break
-	if first_p==-1 or last_p==-1:
-		print(trig,words)
+	if last_p==-1:
+		for i in range(len(words)):
+			if trig[-1] in words[i] or words[i] in trig[-1]:
+				last_p = i
+				break
 	return [i - pos for i in range(len(words))],[first_p,last_p]
-
 
 tweet_pairs = []
 distance_vecs = []
@@ -39,10 +46,13 @@ with open(file_path, 'r') as f:
 		first_last_pos.append(pos)
 		tot_data.append(data)
 	for i in range(len(tot_data)):
-		for j in range(len(tot_data)):
+		for j in range(i+1,len(tot_data)):
 			if abs(int(tot_data[i][4]) -
 				   int(tot_data[j][4])) <= pair_up_threshold:
-				tweet_pairs.append([i, j])
+				label = 0
+				if tot_data[i][1] == tot_data[j][1]:
+					label = 1
+				tweet_pairs.append([i, j, label])
 
 minv = 200
 for i in distance_vecs:
